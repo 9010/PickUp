cb06285cabb4bc03190f81e5d029c91ac6915a93
zx <1106690435@qq.com>
2019-10-18 16:30:11 +0800

    test config server

--- pickup-config/src/main/java/com/self/pickup/config/PickupConfigApplication.java
+++ pickup-config/src/main/java/com/self/pickup/config/PickupConfigApplication.java
@@ -2,8 +2,13 @@ package com.self.pickup.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableConfigServer
 public class PickupConfigApplication {
 
     public static void main(String[] args) {
