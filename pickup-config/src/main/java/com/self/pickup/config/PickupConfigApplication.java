b329e9e67d38c426a965c18e108317e901399fc6
zx <1106690435@qq.com>
2019-10-18 16:14:17 +0800

    init config server

---
+++ pickup-config/src/main/java/com/self/pickup/config/PickupConfigApplication.java
@@ -0,0 +1,13 @@
+package com.self.pickup.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickupConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickupConfigApplication.class, args);
+    }
+
+}
---
+++ pickup-config/src/test/java/com/self/pickup/config/PickupConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.pickup.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickupConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
