71c6e71477f867ec0f336bf71d0787c8ee3efe23
zx <1106690435@qq.com>
2019-10-18 16:54:06 +0800

    test config server

--- pickup-config/src/main/java/com/self/pickup/config/PickupConfigApplication.java
+++ pickup-config/src/main/java/com/self/pickup/config/PickupConfigApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 @EnableConfigServer
 public class PickupConfigApplication {
