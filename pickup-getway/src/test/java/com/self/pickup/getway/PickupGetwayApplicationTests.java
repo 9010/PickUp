2947c46bbe8c173bd93b58f7a81f9649904c79ab
zx <1106690435@qq.com>
2019-10-18 10:33:45 +0800

    init getway

---
+++ pickup-getway/src/test/java/com/self/pickup/getway/PickupGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.pickup.getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickupGetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
