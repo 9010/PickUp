524c0f0778c4bc367791432d267cec77ab618b4d
zx <1106690435@qq.com>
2019-10-18 14:55:42 +0800

    init

---
+++ pickup-eureka/src/main/java/com/self/pickup/eureka/PickupEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.self.pickup.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickupEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickupEurekaApplication.class, args);
+    }
+
+}
---
+++ pickup-eureka/src/test/java/com/self/pickup/eureka/PickupEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.pickup.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickupEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
