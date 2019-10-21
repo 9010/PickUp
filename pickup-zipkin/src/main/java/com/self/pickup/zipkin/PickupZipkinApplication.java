3268e1264dfbc4ca19b6733d59cfc50b94e5e311
zx <1106690435@qq.com>
2019-10-21 14:10:00 +0800

    废弃自建zipkin

--- pickup-zipkin/src/main/java/com/self/pickup/zipkin/PickupZipkinApplication.java
+++ pickup-zipkin/src/main/java/com/self/pickup/zipkin/PickupZipkinApplication.java
@@ -3,7 +3,9 @@ package com.self.pickup.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin2.server.internal.EnableZipkinServer;
 
+@EnableZipkinServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class PickupZipkinApplication {
