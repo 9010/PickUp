73fa71e5359ca4534f0331d6676e113d192aed7e
zx <1106690435@qq.com>
2019-10-21 11:05:06 +0800

    create zipkin

---
+++ pickup-zipkin/src/main/java/com/self/pickup/zipkin/PickupZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.self.pickup.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickupZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickupZipkinApplication.class, args);
+	}
+
+}
---
+++ pickup-zipkin/src/test/java/com/self/pickup/zipkin/PickupZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.pickup.zipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickupZipkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
