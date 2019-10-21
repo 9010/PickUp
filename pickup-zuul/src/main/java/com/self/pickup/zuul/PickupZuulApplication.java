bb265f38329d526f8ac9b38df2d3af7659d07180
zx <1106690435@qq.com>
2019-10-21 14:27:28 +0800

    create zuul

---
+++ pickup-zuul/src/main/java/com/self/pickup/zuul/PickupZuulApplication.java
@@ -0,0 +1,13 @@
+package com.self.pickup.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickupZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickupZuulApplication.class, args);
+    }
+
+}
