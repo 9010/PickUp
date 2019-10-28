7f277be527e47f90309d281d74b20366aa121649
zx <1106690435@qq.com>
2019-10-28 12:02:50 +0800

    create pickup-provider-user

---
+++ pickup-provider/pickup-provider-user/src/main/java/com/self/pickup/provider/user/PickupProviderUserApplication.java
@@ -0,0 +1,13 @@
+package com.self.pickup.provider.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickupProviderUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickupProviderUserApplication.class, args);
+    }
+
+}
---
+++ pickup-provider/pickup-provider-user/src/test/java/com/self/pickup/provider/user/PickupProviderUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.pickup.provider.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickupProviderUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
