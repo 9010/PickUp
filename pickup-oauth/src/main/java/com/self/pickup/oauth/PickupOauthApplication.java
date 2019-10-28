d6591e1b7369807f8e8cdd6541e669274eb6c3e4
zx <1106690435@qq.com>
2019-10-28 10:08:34 +0800

    create pickup-oauth

---
+++ pickup-oauth/src/main/java/com/self/pickup/oauth/PickupOauthApplication.java
@@ -0,0 +1,13 @@
+package com.self.pickup.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickupOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickupOauthApplication.class, args);
+    }
+
+}
