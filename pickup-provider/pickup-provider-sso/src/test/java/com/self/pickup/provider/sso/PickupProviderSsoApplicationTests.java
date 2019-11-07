d42ece0e3e36fde9ce4b115012e98770a02f3f71
zx <1106690435@qq.com>
2019-11-07 16:24:05 +0800

    sso

---
+++ pickup-provider/pickup-provider-sso/src/test/java/com/self/pickup/provider/sso/PickupProviderSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.pickup.provider.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickupProviderSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
