c988e2528e46d092748d94b628d724a0e40e34e3
zx <1106690435@qq.com>
2019-11-07 13:36:38 +0800

    change

---
+++ pickup-oauth/src/main/java/com/self/pickup/oauth/domain/PickupUser.java
@@ -0,0 +1,165 @@
+package com.self.pickup.oauth.domain;
+
+import javax.persistence.*;
+
+@Table(name = "pickup_user")
+public class PickupUser {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "SELECT LAST_INSERT_ID()")
+    private Integer id;
+
+    /**
+     * 账号
+     */
+    private String account;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 推送token
+     */
+    private String token;
+
+    /**
+     * 学校ID
+     */
+    @Column(name = "school_id")
+    private String schoolId;
+
+    /**
+     * 身份信息
+     */
+    @Column(name = "credit_id")
+    private String creditId;
+
+    /**
+     * 是否被注销 0代表未注销 1代表已注销
+     */
+    private Boolean removed;
+
+    /**
+     * @return ID
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取账号
+     *
+     * @return account - 账号
+     */
+    public String getAccount() {
+        return account;
+    }
+
+    /**
+     * 设置账号
+     *
+     * @param account 账号
+     */
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    /**
+     * 获取密码
+     *
+     * @return password - 密码
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置密码
+     *
+     * @param password 密码
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 获取推送token
+     *
+     * @return token - 推送token
+     */
+    public String getToken() {
+        return token;
+    }
+
+    /**
+     * 设置推送token
+     *
+     * @param token 推送token
+     */
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    /**
+     * 获取学校ID
+     *
+     * @return school_id - 学校ID
+     */
+    public String getSchoolId() {
+        return schoolId;
+    }
+
+    /**
+     * 设置学校ID
+     *
+     * @param schoolId 学校ID
+     */
+    public void setSchoolId(String schoolId) {
+        this.schoolId = schoolId;
+    }
+
+    /**
+     * 获取身份信息
+     *
+     * @return credit_id - 身份信息
+     */
+    public String getCreditId() {
+        return creditId;
+    }
+
+    /**
+     * 设置身份信息
+     *
+     * @param creditId 身份信息
+     */
+    public void setCreditId(String creditId) {
+        this.creditId = creditId;
+    }
+
+    /**
+     * 获取是否被注销 0代表未注销 1代表已注销
+     *
+     * @return removed - 是否被注销 0代表未注销 1代表已注销
+     */
+    public Boolean getRemoved() {
+        return removed;
+    }
+
+    /**
+     * 设置是否被注销 0代表未注销 1代表已注销
+     *
+     * @param removed 是否被注销 0代表未注销 1代表已注销
+     */
+    public void setRemoved(Boolean removed) {
+        this.removed = removed;
+    }
+}
---
+++ pickup-oauth/src/main/java/com/self/pickup/oauth/mapper/PickupUserMapper.java
@@ -0,0 +1,7 @@
+package com.self.pickup.oauth.mapper;
+
+import com.self.pickup.common.myMapper.MyMapper;
+import com.self.pickup.oauth.domain.PickupUser;
+
+public interface PickupUserMapper extends MyMapper<PickupUser> {
+}
