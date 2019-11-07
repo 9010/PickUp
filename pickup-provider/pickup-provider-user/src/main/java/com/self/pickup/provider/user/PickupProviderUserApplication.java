f8f3d99253fb44ec4e06c219b3ec45159e4d4b07
zx <1106690435@qq.com>
2019-11-07 15:21:29 +0800

    放弃oauth2

--- pickup-provider/pickup-provider-user/src/main/java/com/self/pickup/provider/user/PickupProviderUserApplication.java
+++ pickup-provider/pickup-provider-user/src/main/java/com/self/pickup/provider/user/PickupProviderUserApplication.java
@@ -2,10 +2,8 @@ package com.self.pickup.provider.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableOAuth2Sso
 @EnableDiscoveryClient
 @SpringBootApplication
 public class PickupProviderUserApplication {
