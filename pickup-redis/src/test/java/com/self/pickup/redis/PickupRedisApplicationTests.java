d473aaa436933cc05e1be9a4687e9f8a591dc60d
zx <1106690435@qq.com>
2019-11-07 15:39:47 +0800

    redis

---
+++ pickup-redis/src/test/java/com/self/pickup/redis/PickupRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.pickup.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickupRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
