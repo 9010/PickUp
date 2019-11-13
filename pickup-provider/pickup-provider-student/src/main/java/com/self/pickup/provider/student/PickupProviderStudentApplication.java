3bdcf6873bf41f83cc5ff0a7ccc6b1ff41853ae0
zx <1106690435@qq.com>
2019-11-13 17:01:28 +0800

    建立provider-student微服务

---
+++ pickup-provider/pickup-provider-student/src/main/java/com/self/pickup/provider/student/PickupProviderStudentApplication.java
@@ -0,0 +1,13 @@
+package com.self.pickup.provider.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickupProviderStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickupProviderStudentApplication.class, args);
+    }
+
+}
---
+++ pickup-provider/pickup-provider-student/src/test/java/com/self/pickup/provider/student/PickupProviderStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.pickup.provider.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickupProviderStudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
