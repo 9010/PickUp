f1070d2cea31b996fbd0dd11176b6b717f379b56
zx <1106690435@qq.com>
2019-11-13 17:03:29 +0800

    初始化provider-student微服务

---
+++ pickup-provider/pickup-provider-student/src/main/java/com/self/pickup/provider/student/mapper/StudentMapper.java
@@ -0,0 +1,7 @@
+package com.self.pickup.provider.student.mapper;
+
+import com.self.pickup.common.myMapper.MyMapper;
+import com.self.pickup.provider.student.domain.Student;
+
+public interface StudentMapper extends MyMapper<Student> {
+}
