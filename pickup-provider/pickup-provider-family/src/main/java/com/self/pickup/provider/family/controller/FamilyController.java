f111bb81903ab33b92a982752d104ca6524d5e03
zx <1106690435@qq.com>
2019-11-15 14:44:09 +0800

    初始化family微服务

---
+++ pickup-provider/pickup-provider-family/src/main/java/com/self/pickup/provider/family/controller/FamilyController.java
@@ -0,0 +1,7 @@
+package com.self.pickup.provider.family.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FamilyController {
+}
---
+++ pickup-provider/pickup-provider-family/src/main/java/com/self/pickup/provider/family/service/GetParentService.java
@@ -0,0 +1,4 @@
+package com.self.pickup.provider.family.service;
+
+public interface GetParentService {
+}
